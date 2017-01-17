c07ea522dc58b8f55761ee4ffc2396b035d547f3
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-01-17 17:11:22 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl3011/recycleview1/adapter/Hotel.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.xirpl3011.recycleview1.adapter;
+
+import android.graphics.drawable.Drawable;
+/**
+ * Created by Smktelkom on 1/17/2017.
+ */
+public class Hotel {
+      public String judul;
+       public String deskripsi;
+      public Drawable foto;
+               public Hotel(String judul, String deskripsi, Drawable foto){
+               this.judul = judul;
+               this.deskripsi = deskripsi;
+               this.foto = foto;
+           }
+    }

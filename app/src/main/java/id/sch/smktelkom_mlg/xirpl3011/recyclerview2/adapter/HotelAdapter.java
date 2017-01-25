b861769737c0128817f586769e320eafe6424453
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-01-25 21:28:52 +0700

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/xirpl3011/recyclerview2/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl3011/recyclerview2/adapter/HotelAdapter.java
@@ -1,13 +1,13 @@
 package id.sch.smktelkom_mlg.xirpl3011.recyclerview2.adapter;
 
 import android.support.v7.widget.RecyclerView;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 import id.sch.smktelkom_mlg.xirpl3011.recyclerview2.R;
 
@@ -33,7 +33,6 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         public void onBindViewHolder(ViewHolder holder, int position) {
                 Hotel hotel = hotelList.get(position);
                 holder.tvJudul.setText(hotel.judul);
-                holder.tvDeskripsi.setText(hotel.deskripsi);
                 holder.ivFoto.setImageDrawable(hotel.foto);
             }
 
@@ -47,13 +46,11 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
                 public class ViewHolder extends RecyclerView.ViewHolder {
                 ImageView ivFoto;
                 TextView tvJudul;
-                TextView tvDeskripsi;
 
                         public ViewHolder(View itemView) {
                         super(itemView);
                         ivFoto = (ImageView) itemView.findViewById(R.id.ImageView);
                         tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
-                        tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
                     }
             }
     }
